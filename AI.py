33a33dd9c39520c51a100574eb12eb6bf0a9f573
marcogracie <marco.gracie22@gmail.com>
2024-10-24 16:34:05 -0400

    feat(IP - 03): Added pulling state to AI class.

--- AI.py
+++ AI.py
@@ -1,7 +1,17 @@
+import main
 from main import *
+from PIL import Image
+import numpy as np
 class AI():
 
-def __init__(self):
-    state = None
+    def __init__(self):
+        state = None
 
-    def()
+    def get_state(self):
+        main.export_window()
+        img = Image.open('state.png', 'r')
+        self.state = np.array(img)
+
+ai = AI()
+ai.get_state()
+print(ai.state)
